074045966c2777d98ec9058da1fca2cc5017fe4e
Vivek Bhalodiya <vivek.bhalodiya@mutualmobile.com>
2019-11-19 00:18:14 +0530

    created BaseViewModel and added ViewModelFactory

---
+++ app/src/main/java/com/vivekbhalodiya/githubtrendingrepos/ui/base/BaseViewModel.kt
@@ -0,0 +1,17 @@
+/*
+ * Created by Vivek Bhalodiya on 19/11/19 12:11 AM
+ * Copyright (c) 2019 . All rights reserved.
+ * Last modified 19/11/19 12:11 AM
+ */
+
+package com.vivekbhalodiya.githubtrendingrepos.ui.base
+
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Vivek Patel on 2019-11-19.
+ */
+abstract class BaseViewModel : ViewModel(), LifecycleObserver {
+
+}

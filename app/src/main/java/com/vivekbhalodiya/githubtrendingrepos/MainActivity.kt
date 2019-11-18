4b02d682f98bf8e348c58e7d0655ac7c44a2b024
Vivek Bhalodiya <vivek.bhalodiya@mutualmobile.com>
2019-11-18 19:04:14 +0530

    added copyright profile

--- app/src/main/java/com/vivekbhalodiya/githubtrendingrepos/MainActivity.kt
+++ app/src/main/java/com/vivekbhalodiya/githubtrendingrepos/MainActivity.kt
@@ -1,3 +1,9 @@
+/*
+ * Created by Vivek Bhalodiya on 18/11/19 6:59 PM
+ * Copyright (c) 2019 . All rights reserved.
+ * Last modified 18/11/19 6:53 PM
+ */
+
 package com.vivekbhalodiya.githubtrendingrepos
 
 import androidx.appcompat.app.AppCompatActivity

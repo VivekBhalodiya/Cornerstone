287135fa49705679a5ee47875252127aaa7aa354
Vivek Bhalodiya <vivek.bhalodiya@mutualmobile.com>
2019-11-19 01:28:49 +0530

    refactored MainActivity to HomeActivity and created HomeViewModel

---
+++ app/src/main/java/com/vivekbhalodiya/githubtrendingrepos/ui/home/HomeActivity.kt
@@ -0,0 +1,19 @@
+/*
+ * Created by Vivek Bhalodiya on 19/11/19 1:25 AM
+ * Copyright (c) 2019 . All rights reserved.
+ * Last modified 18/11/19 7:01 PM
+ */
+
+package com.vivekbhalodiya.githubtrendingrepos.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vivekbhalodiya.githubtrendingrepos.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/vivekbhalodiya/githubtrendingrepos/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+/*
+ * Created by Vivek Bhalodiya on 19/11/19 1:26 AM
+ * Copyright (c) 2019 . All rights reserved.
+ * Last modified 19/11/19 1:26 AM
+ */
+
+package com.vivekbhalodiya.githubtrendingrepos.ui.home
+
+/**
+ * Created by Vivek Patel on 2019-11-19.
+ */
+class HomeViewModel {
+}
